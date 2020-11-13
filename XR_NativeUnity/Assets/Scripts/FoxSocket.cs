9b4f6da7bc6b61081d54e512b789eef18e10e997
Mark Suter <msuter@live.elida.k12.oh.us>
2020-11-13 13:43:22 -0500

    Experiementing with Socket limiting to objects

---
+++ XR_NativeUnity/Assets/Scripts/FoxSocket.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoxSocket : MonoBehaviour
+{
+    public void IsItAFox(Collider other)
+    {
+        if (other.tag == "fox")
+        {
+            Debug.Log("Yes, it's a fox hovering");
+        }
+        else
+        {
+            Debug.Log("NOT A FOX HOVERING");
+        }
+        
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
