9375afc0f5c3f7e248689acc6f0b2c9b385082e4
Mark Suter <msuter@live.elida.k12.oh.us>
2020-11-13 14:40:08 -0500

    Overriding On Hover Enter event with script

--- XR_NativeUnity/Assets/Scripts/FoxSocket.cs
+++ XR_NativeUnity/Assets/Scripts/FoxSocket.cs
@@ -4,18 +4,31 @@ using UnityEngine;
 
 public class FoxSocket : MonoBehaviour
 {
-    public void IsItAFox(Collider other)
+
+    private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "fox")
         {
-            Debug.Log("Yes, it's a fox hovering");
+            Debug.Log("fox is here.");
         }
         else
         {
             Debug.Log("NOT A FOX HOVERING");
         }
-        
     }
+
+    //public void IsItAFox(Collider other)
+    //{
+    //    if (other.gameObject.tag == "fox")
+    //    {
+    //        Debug.Log("Yes, it's a fox hovering");
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("NOT A FOX HOVERING");
+    //    }
+
+    //}
 
     void Start()
     {
