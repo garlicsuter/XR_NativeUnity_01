a919fc964fd1dd781f1cb92924e499622c7b376b
Mark Suter <msuter@live.elida.k12.oh.us>
2020-11-16 09:05:35 -0500

    ball returner added (bowling test)

---
+++ XR_NativeUnity/Assets/Scripts/BallReturn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallReturn : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        //Check to see if the tag on the collider is equal to Enemy
+        if (other.tag == "ballreturn")
+        {
+            Debug.Log("hit ball return");
+            transform.position = new Vector3(.31f, 1.06f, 0.062f);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
